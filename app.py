a8ff783a22fe063e74aab952d35a6535d12af7e3
arkadev658 <76399013+arkadev658@users.noreply.github.com>
2024-02-01 13:50:09 +0530

    Update app.py
    cropping issue fixed

--- app.py
+++ app.py
@@ -1,27 +1,31 @@
 import streamlit as st
+from streamlit_cropper import st_cropper
+from PIL import Image, ImageDraw
 import cv2
 import numpy as np
 
-
-
 def process_image(image):
+    # Define bacteria characteristics
     bact = [[60, 100, 10], [1, 1, 253], [80, 18, 10]]
     bact_name = ['bact_x', 'bact_y', 'bact_z']
 
-    img = cv2.imdecode(np.fromstring(image.read(), np.uint8), cv2.IMREAD_UNCHANGED)
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    # Convert PIL Image to OpenCV format (BGR)
+    img = np.array(image)
 
-    if img is None:
-        st.warning("Unable to read the uploaded image. Please make sure it is a valid image file.")
-        return None
+    if img.shape[-1] == 3:
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
+    # Convert the image to grayscale
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    mean_brightness = np.mean(gray)
-    brightness_threshold = 128
 
+    # Apply bilateral filtering
     gray_blurred = cv2.bilateralFilter(gray, d=9, sigmaColor=100, sigmaSpace=75)
+
+    # Thresholding to create a binary mask
     roi_mask = cv2.adaptiveThreshold(gray_blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 2)
 
+    # Find contours in the binary mask
     contours, _ = cv2.findContours(roi_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     l = 0
 
@@ -87,6 +91,11 @@ def process_image(image):
     return img
 
 
+# def _resize_img(img_file):
+#     # Use PIL to open the image and get its size
+#     img = Image.open(img_file)
+#     return img
+
 def main():
     st.title("Bacteria Detection App")
     st.write("Upload an image, and the app will process it to detect bacteria.")
@@ -96,12 +105,22 @@ def main():
     if uploaded_image is not None:
         st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
 
-        if st.button("Process Image"):
-            # Process the image
-            processed_img = process_image(uploaded_image)
+        st.subheader("Manual Crop")
+        resized_img = st_cropper(Image.open(uploaded_image), realtime_update=True, aspect_ratio=(1, 1))
+
+        if resized_img is not None:
+            st.image(resized_img, caption="Cropped Image", use_column_width=True)
+
+            if st.button("Process Cropped Image"):
+                processed_img = process_image(resized_img)
+
+                if processed_img is not None:
+                    st.image(processed_img, caption="Processed Image", use_column_width=True)
+
+        if st.button("Process Entire Image"):
+            processed_img = process_image(Image.open(uploaded_image))
 
             if processed_img is not None:
-                # Display the processed image
                 st.image(processed_img, caption="Processed Image", use_column_width=True)
 
 
